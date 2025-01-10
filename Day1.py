4506047b8c90d48d9d5ded045f6387a891e4c0dc
Waris Hayat <122077609+Warishayat@users.noreply.github.com>
2025-01-10 23:01:12 +0500

    day1

---
+++ Day1.py
@@ -0,0 +1,42 @@
+# print("Hello world")
+
+# #creat variable 
+# a  =  1
+# if (a==1):
+#     print(True)
+# else:
+#     print(False)
+
+
+# #take user input 
+# name= input("Enter the name:")
+# print("Your username is :",name)
+
+# #generata table
+
+# number = int(input("Generate the input number = "))
+
+# for i in range(1,11,1):
+#     print(number,"*", i, "=", number*i)
+
+# #loop
+# number = 5
+# for i in range(1,number):
+#     print(i)
+
+#list
+name = ["uzair","asim","ali","numan","ali"]
+
+# for i in name:
+#     print("The name of the person is:",i)
+
+# name.append("waris")
+
+# for i in name:
+#     print('name is:',i,end=" ")
+
+
+# print(f"the {named} is popped")
+
+#count special name
+print(name.count("ali"))
